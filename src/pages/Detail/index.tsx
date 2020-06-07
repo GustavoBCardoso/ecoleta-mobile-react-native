c2d3647b502d4723d8b94bc10ff544c708963dc5
Gustavo Borges <gustavob68@gmail.com>
2020-06-07 12:38:32 -0300

    Finalizando page Home

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+
+
+const Detail = () => {
+    return <View/>;
+}
+
+export default Detail;
---
+++ src/pages/Points/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+
+
+const Points = () => {
+    return <View/>;
+}
+
+export default Points;
