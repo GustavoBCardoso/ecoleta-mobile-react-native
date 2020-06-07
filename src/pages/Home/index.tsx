76fef08f128323360c78c476acd85f231d8effb4
Gustavo Borges <gustavob68@gmail.com>
2020-06-07 19:38:05 -0300

    Finalizando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
-import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { View, ImageBackground, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+    const [uf, setUf] = useState('');
+    const [city, setCity] = useState('');
     const navigation = useNavigation();
 
     function handleNavigateToPoints() {
-        navigation.navigate('Points');
+        navigation.navigate('Points', {
+          uf,
+          city,
+        });
     }
 
     return (
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
         <ImageBackground 
             source={require('../../assets/home-background.png')} 
             style={styles.container}
@@ -19,11 +25,30 @@ const Home = () => {
         >
             <View style={styles.main}>
                 <Image source={require('../../assets/logo.png')} />
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+                <View>
+                  <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                  <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+                </View>
             </View>
             
-            <View style={styles.footer}> 
+            <View style={styles.footer}>
+                <TextInput 
+                  style={styles.input}
+                  placeholder="Digite a UF"
+                  value={uf}
+                  maxLength={2}
+                  autoCapitalize="characters"
+                  autoCorrect={false}
+                  onChangeText={setUf}
+                />
+                <TextInput 
+                  style={styles.input}
+                  placeholder="Digite a Cidade"
+                  value={city}
+                  autoCorrect={false}
+                  onChangeText={setCity}
+                />
+
                 <RectButton  style={styles.button} onPress={handleNavigateToPoints}>
                     <View style={styles.buttonIcon}>
                         <Text>
@@ -35,8 +60,8 @@ const Home = () => {
                     </Text>
                 </RectButton>
             </View>
-
         </ImageBackground>
+      </KeyboardAvoidingView>
     );
 }
 
